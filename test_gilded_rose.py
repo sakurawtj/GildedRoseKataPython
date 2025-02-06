12087d057a07e5e389c886d7244fcc811f93190c
sakurawtj <53387760+sakurawtj@users.noreply.github.com>
2025-02-05 20:43:25 -0800

    add more tests for lab4

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -22,6 +22,52 @@ class GildedRoseTest(unittest.TestCase):
         all_items = gilded_rose.get_items()
         self.assertEquals(["Sulfuras"], all_items)
 
+    # example1 of test that checks for logical errors
+    def test_Backstage_passes_quality_increase_by_2_less_10_days(self):
+        items = [Item("Backstage passes", 8, 25)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        backstage_item = items[0]
+        self.assertEquals(27, backstage_item.quality)
+        self.assertEquals(7, backstage_item.sell_in)
+
+    # example2 of test that checks for logical errors
+    def test_Backstage_passes_quality_to_0_when_expired(self):
+        items = [Item("Backstage passes", 1, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        backstage_item = items[0]
+        self.assertEquals(0, backstage_item.quality)
+
+    # example3 of test that checks for logical errors
+    def test_Conjured_item_decrease_quality_twice(self):
+        items = [Item("Conjured Regular item", 5, 22)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        backstage_item = items[0]
+        self.assertEquals(20, backstage_item.quality)
+        self.assertEquals(4, backstage_item.sell_in)
+
+    # example1 of test that checks for syntax errors
+    def test_remove_all_quality_0(self):
+        items = [
+            Item(name="product1", sell_in=0, quality=0),
+            Item(name="product2", sell_in=0, quality=0),
+            Item(name="product3", sell_in=5, quality=20),
+            Item(name="product2", sell_in=6, quality=10),
+        ]
+        gilded_rose = GildedRose(items)
+        items = gilded_rose.remove_quality_zero()
+        self.assertEquals(len(items), 2)
+
+    # example4 of test that checks for logical errors
+    def test_Backstage_passes_quality_increase_by_3_less_5_days(self):
+        items = [Item("Backstage passes", 5, 30)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        backstage_item = items[0]
+        self.assertEquals(33, backstage_item.quality)
+        self.assertEquals(4, backstage_item.sell_in)
 
 
 if __name__ == '__main__':
